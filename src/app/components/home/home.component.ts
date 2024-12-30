450ac44cbd8b32493ab918e4311ffc3c564ef383
YogeshNIKQIK <yogesh.mahajan@nikqik.com>
2024-12-30 16:56:50 +0530

    new changes done on 30 dec 2024

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -20,7 +20,7 @@ export class HomeComponent {
   ngOnInit() {
 
     this.enqueryForm = this.formBuilder.group({
-      fullname: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z ]+')]),
+      fname: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z ]+')]),
       email: new FormControl ('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
       phone: new FormControl('', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]),
       subject: new FormControl('', Validators.required),
@@ -36,11 +36,15 @@ export class HomeComponent {
     } else {
       this.submitted = false;
       const formData = this.enqueryForm.value;
-      console.log(formData);
-      formData['enqueryform'] = 2;
+      const data = JSON.stringify(formData);
+        let formBody = {
+          "body": data
+        }
+      // console.log(formData);
+      // formData['enqueryform'] = 2;
 
       axios
-        .post(`https://nikqik.manageprojects.in/api/contact.php`, formData)
+        .post(`https://8p78ugp5i0.execute-api.ap-southeast-2.amazonaws.com/dev`, formBody)
         .then(response => {
           this.toastr.success('Message Sent Successfully'); // Show success toast
           this.enqueryForm.reset();
@@ -64,7 +68,7 @@ export class HomeComponent {
    slides = [
      { img: 'assets/images/forum_11876045.png', title: 'ITSM Process Consulting', description: 'Imagine your IT operations running smoothly, efficiently, and seamlessly aligned with your business goals.', link: 'services-detail#it-service-management-process-consulting' },
      { img: 'assets/images/1641623.webp', title: 'ITSM Tools Consulting & Implementation', description: 'Streamlining Your Operations with Cutting-edge IT Service Management Tools', link: 'services-detail#it-service-management-tools-implementation' },
-     { img: 'assets/images/cloud_14217783.png', title: 'Cloud Center Professional Services', description: 'In today digital world, exceptional customer experience is the key to loyalty and growth.', link: 'services-detail#cloud-contact-center-professional-services' },
+     { img: 'assets/images/cloud_14217783.png', title: 'Cloud Consulting Professional Services', description: 'In today digital world, exceptional customer experience is the key to loyalty and growth.', link: 'services-detail#cloud-contact-center-professional-services' },
      { img: 'assets/images/high-quality_5733207.png', title: 'Quality Assurance', description: 'In today fast-paced world, delivering quality isnt just optional, its essential.', link: 'services-detail#quality-assuarance' },
      { img: 'assets/images/process_10740747.png', title: 'Project Management', description: 'Every successful project starts with a vision', link: 'services-detail#project-management' },
      { img: 'assets/images/data-analytics_1548914.png', title: 'Data Analytics', description: 'In today digital world, data is a treasure trove of insights, but only if you have the keys to unlock its potential.', link: 'services-detail#data-analytics' },
@@ -72,6 +76,7 @@ export class HomeComponent {
      { img: 'assets/images/statistics_7984698.png', title: 'Business Analysis', description: 'In today competitive landscape, success hinges on making informed decisions.', link: 'services-detail#business-analysis' },
      { img: 'assets/images/group_598523.png', title: 'Staff Augmentation', description: 'Filling talent gaps can feel like trying to plug a leak on a moving boat.', link: 'services-detail#staff-augmentation' },
      { img: 'assets/images/global-services_12876196.png', title: 'Managed Services', description: 'In todayfast-paced world, running a business means a million demands on your time.', link: 'services-detail#managed-services' },
+     { img: 'assets/images/web-app-development.png', title: 'Web & Custom App Development', description: 'Ready to elevate your digital presence to new heights?', link: 'services-detail#web-app-development' },
   ];
 
   slideConfig = {
